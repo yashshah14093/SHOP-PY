80f96a202a7cb77c88fa10492407ee268eb6533b
yashshah14093 <2018390@iiitdmj.ac.in>
2020-08-08 13:24:08 +0530

    Added scripts for automating mail to supplier

---
+++ scripts/reports.py
@@ -0,0 +1,44 @@
+import os
+import datetime
+from reportlab.platypus import SimpleDocTemplate
+from reportlab.platypus import Paragraph, Spacer, Table, image
+from reportlab.lib import colors
+
+
+
+def create_dict(path):
+    data = {"name" : "weight"}
+    for file in os.listdir(path):
+    	with open(path+file) as f:
+    		input = f.readline
+    		name = input()
+    		weight = input()
+    		data[name] = weight
+    return data
+
+
+
+def generate_report(path):
+
+	data_report = create_dict(path)
+
+	date = datetime.datetime.now()
+
+
+	path_to_report = os.path.expanduser('~') + "/supplier-data/report.pdf"
+    report = SimpleDocTemplate(path_to_report)
+    styles = getSampleStyleSheet()
+
+    title = "Processed Update on " + date.strftime("%B %d,%Y")
+    report_title = Paragraph(title, styles["h1"])
+
+
+    table_data = []
+    for key,value in data_report:
+    	table_data.append([key,value])
+    table_style = [('GRID', (0,0), (-1,-1), 1, colors.black)]
+    report_table = Table(data = table_data, style = table_style, hAlign = "CENTER")
+
+    report.build([report_title,report_table])
+
+    return path_to_report
