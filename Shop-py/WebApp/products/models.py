c22417510c7f03d46d5698c1b32dffc1255893fd
yashshah14093 <2018390@iiitdmj.ac.in>
2020-08-26 12:12:13 +0530

    Added Media_urls and Media_Root in settings.py

--- Shop-py/WebApp/products/models.py
+++ Shop-py/WebApp/products/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Product(models.Model):
     Name = models.CharField(max_length=200,null = True)
     Quantity = models.IntegerField(null = True)
-    Image = models.FilePathField(path = "/img")
+    Image = models.FilePathField(path = "img/")
     Description = models.TextField()
 
 
--- Shop-py/WebApp/products/urls.py
+++ Shop-py/WebApp/products/urls.py
@@ -2,8 +2,13 @@ from django.urls import path
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path("", views.prod_index, name="prod_index"),
     path("index",views.show_list,name="GET_POST_REQUEST"),
-]+ static(settings.STATIC_URL , document_root = settings.STATIC_ROOT)
+]
+
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
